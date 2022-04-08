bad67b1ddf750592695104194ebac6d0331cf5f2
MbankoKevin <kelvinemveli@gmail.com>
2022-04-08 20:29:07 +0100

    First commit

---
+++ clip.py
@@ -0,0 +1,8 @@
+import sys
+import json
+
+
+
+if len(sys.argv) == 2:
+    cmd = sys.argv[1]
+    print(cmd)
---
+++ index.py
@@ -0,0 +1,27 @@
+#def func(word, add=5, freq=1):
+ #   print(word* (add+freq) )
+    
+   
+#call = func('king')
+#print(call)
+
+
+#from multiprocessing import Condition
+
+
+"""class car(object):
+    def __init__(self, make, model, year, condition, kms):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.condition = condition
+        self.kms = kms
+    def display(self, showAll):
+        if showAll:
+            print("This car is %s %s from %s, it is %s and has %s kms." %(self.make, self.model, self.year, self.condition,self.kms))
+        else:
+            print("This car is a %s %s from %s." %(self.make, self.model, self.year))
+
+whip = car('Ford', 'Fusion', 2012, 'NEW', 0)
+whip.display(True)"""
+            
